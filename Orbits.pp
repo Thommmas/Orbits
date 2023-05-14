64f6720a85eea57c561bf418adcaf8c6cd1c60c3
Schiltz Thomas <thomas.schiltz@student.uclouvain.be>
2023-05-15 00:51:44 +0200

    Switch to Pascal

---
+++ Orbits.pp
@@ -0,0 +1,26 @@
+program Orbits;
+uses crt,graph;
+var grmode, grdriver, x , i:integer;
+
+begin
+
+  clrscr;
+  grdriver := DETECT;
+
+  initgraph(grdriver,grmode, ' ' );
+  
+for i:= 1 to 10 do
+    begin
+      for x := 0 to 360 do 
+        begin
+        ClearDevice;
+        ellipse(1000,500,x,x+10,125,75);
+
+        
+        end;
+
+    end; 
+    readln;
+    CloseGraph;
+    
+end.
