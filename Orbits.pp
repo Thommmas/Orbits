a70166a02616f614d15607ba816db20936094615
Schiltz Thomas <thomas.schiltz@student.uclouvain.be>
2023-05-23 00:09:59 +0200

    Update

--- Orbits.pp
+++ Orbits.pp
@@ -3,25 +3,20 @@ uses crt,graph;
 var grmode, grdriver, x , i:integer;
 
 begin
+  write(GetViewSettings(var ViewPortType));
+
 
   clrscr;
   grdriver := DETECT;
-
+  SetViewPort(0,0,500,500,True);
   initgraph(grdriver,grmode, ' ' );
-  
-for i:= 1 to 10 do
-    begin
-      for x := 0 to 360 do 
-        begin
-        ClearDevice;
-        {ellipse(x1,y1,degreesfrom,degrees to,xr,yr)}
-        ellipse(1000,500,x,x+10,125,75);
 
-        
-        end;
+  setviewport(100,100,500,400,true);
 
-    end; 
-    readln;
-    CloseGraph;
+
+
+
+  readln;
+  CloseGraph;
     
 end.
